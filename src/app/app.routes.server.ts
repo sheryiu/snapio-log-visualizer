52e251796d5a97329c086e0c5c321a8549fbc9d1
Sherlock <1911214+sheryiu@users.noreply.github.com>
2026-01-19 00:54:01 +0800

    feat: github pages

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'visualize/*',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
