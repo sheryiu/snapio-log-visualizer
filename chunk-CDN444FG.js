29c57954956260986b6471ad832e9a98bc569f6d
Sherlock <1911214+sheryiu@users.noreply.github.com>
2026-01-19 00:47:14 +0800

    Auto-generated commit

---
+++ chunk-CDN444FG.js
@@ -0,0 +1,1 @@
+import{Cb as s,Ra as o,mb as m,nb as n}from"./chunk-H274L5YS.js";var a=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-home"]],decls:2,vars:0,template:function(e,p){e&1&&(m(0,"p"),s(1,"home works!"),n())},encapsulation:2})};export{a as Home};
